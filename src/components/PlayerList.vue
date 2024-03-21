d98e278568f84db531993621853c9c217c9a47ad
Jelle Wolbers <jelle.wolbers@tno.nl>
2024-03-21 18:37:10 +0100

    Added support for multiple playerObjects

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -9,18 +9,19 @@ const worldStateStore = useWorldStateStore()
             <h1 class="text-3xl text-gray-600">Player States</h1>
         </div>
         <div v-for="playerState in worldStateStore.playerStates" class="flex items-center justify-between rounded bg-gray-100 p-4">
-            <div class="text-2xl font-bold text-cyan-900">{{ playerState.id }}</div>
+            <div class="text-2xl font-bold text-cyan-900">{{ playerState.clientId }}</div>
             <div class="flex flex-col font-mono text-xl">
-                <div class="flex justify-end gap-8">
-                    <div>{{ playerState.headPosition.x.toFixed(4) }}</div>
-                    <div>{{ playerState.headPosition.y.toFixed(4) }}</div>
-                    <div>{{ playerState.headPosition.z.toFixed(4) }}</div>
-                </div>
-                <div class="flex justify-end gap-8">
-                    <div>{{ playerState.headRotation.w.toFixed(4) }}</div>
-                    <div>{{ playerState.headRotation.x.toFixed(4) }}</div>
-                    <div>{{ playerState.headRotation.y.toFixed(4) }}</div>
-                    <div>{{ playerState.headRotation.z.toFixed(4) }}</div>
+                <div v-for="playerObject in playerState.playerObjects" class="flex justify-end gap-8">
+                    {{ playerObject.id }}
+                    -
+                    {{ playerObject.position.x.toFixed(4) }}
+                    {{ playerObject.position.y.toFixed(4) }}
+                    {{ playerObject.position.z.toFixed(4) }}
+                    -
+                    {{ playerObject.rotation.w.toFixed(4) }}
+                    {{ playerObject.rotation.x.toFixed(4) }}
+                    {{ playerObject.rotation.y.toFixed(4) }}
+                    {{ playerObject.rotation.z.toFixed(4) }}
                 </div>
             </div>
         </div>
